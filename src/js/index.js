d16f2fc6572acc89283fcd6418eb0feca963aee6
mateuszGadecki <mateusz.gadecki98@gmail.com>
2020-09-30 19:21:19 +0200

    Creating DOM controller modile(base.js), display search results in UI, creating clear functions after click search button

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,39 @@
 import Search from './models/Search';
+import * as searchView from './views/searchView';
+import { elements } from './views/base';
 
-const search = new Search('pizza');
-console.log(search);
-search.getResults();
+
+
+/* Global state of the app */
+// Search object
+// Current recipe object
+// Shopping list object
+// Liked recipes
+
+const state = {}
+
+const controlSearch = async () => {
+    // 1. Get query from the view
+    const query = searchView.getInput();
+    console.log(query);
+
+    if(query) {
+        // 2. New search object and add to state
+        state.search = new Search(query);
+
+        // 3. Prepare UI for results
+        searchView.clearInput();
+        searchView.clearResults();
+
+        // 4. Search for recipies
+        await state.search.getResults();
+
+        // 5. Render results on UI
+        searchView.renderResults(state.search.result);
+    }
+}
+
+elements.searchForm.addEventListener('submit', e => {
+    e.preventDefault();
+    controlSearch();
+});
--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -8,8 +8,8 @@ export default class Search {
     async getResults() {
         try {
             const res = await axios(`https://forkify-api.herokuapp.com/api/search?&q=${this.query}`);
-            this.result = res.data.recipies;
-            // console.log(this.result);
+            this.result = res.data.recipes;
+            console.log(this.result);
         } catch(error) {
             alert(error);
         }
