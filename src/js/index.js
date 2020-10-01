6dfdef9fa229fb4b9faa061166a9646e1585ee40
mateuszGadecki <mateusz.gadecki98@gmail.com>
2020-10-01 18:05:13 +0200

    Adding loading spinner into search results section

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,6 @@
 import Search from './models/Search';
 import * as searchView from './views/searchView';
-import { elements } from './views/base';
+import { elements, renderLoader, clearLoader } from './views/base';
 
 
 
@@ -24,8 +24,10 @@ const controlSearch = async () => {
         // 3. Prepare UI for results
         searchView.clearInput();
         searchView.clearResults();
+        renderLoader(elements.searchRes);
 
         // 4. Search for recipies
+        clearLoader();
         await state.search.getResults();
 
         // 5. Render results on UI
